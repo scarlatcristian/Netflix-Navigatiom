e360a897d21f1bfb0544ec9b656178efc27cb673
cristi <scarlat.cristian.gabriel@gmail.com>
2023-06-13 18:36:45 +0300

    added JS and transition-delay

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+const nav = document.querySelectorAll(".nav");
+
+openBtn.addEventListener("click", () => {
+  nav.forEach((nav_el) => nav_el.classList.add("visible"));
+});
+
+closeBtn.addEventListener("click", () => {
+  nav.forEach((nav_el) => nav_el.classList.remove("visible"));
+});
